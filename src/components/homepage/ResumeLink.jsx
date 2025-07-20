1e1f807375c10a07dbf3ab40ffed363f9058f1b0
Gourav Kumar <gouravpanchal1632@gmail.com>
2025-07-20 23:03:38 +0530

    Update resume link

--- src/components/homepage/ResumeLink.jsx
+++ src/components/homepage/ResumeLink.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 
 function ResumeLink() {
   return (
-    <a href="https://drive.google.com/file/d/1k4FeTOcHCcrShEVivJZMOil2moYbJe29/view?usp=sharing" target='_blank' >
+    <a href="https://gourav1632.github.io/Profile/Resume.pdf" target='_blank' >
       <motion.button initial={{opacity:0,y:"-10px"}} animate={{opacity:1,y:"0px"}}  transition={{duration:.3}} whileHover={{scale:1.02}} className=" bg-[#B8001F] cursor-pointer  aspect-square  h-[140px] md:h-[200px] lg:h-full rounded-4xl lg:rounded-[40px] relative flex items-center justify-center">
         <BsArrowUpRight  className="text-[25px] lg:text-[30px] 2xl:text-[40px] absolute top-4 lg:top-4 right-4 lg:right-4"  />
         <span className='text-[20px] lg:text-2xl 2xl:text-3xl italic font-fira'>Resume</span>
